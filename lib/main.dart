72c5c4c154435da9d1e63199c5f9d6280131ce96
Analthakkudooz <analraj2002@gmail.com>
2021-08-22 01:09:03 +0530

    added

---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/salvadores_verdesapp/generatedloadingscreenwidget/GeneratedLoadingscreenWidget.dart';
+import 'package:flutterapp/salvadores_verdesapp/generatedloadingscreenwidget1/GeneratedLoadingscreenWidget1.dart';
+import 'package:flutterapp/salvadores_verdesapp/generatedframe4widget/GeneratedFrame4Widget.dart';
+import 'package:flutterapp/salvadores_verdesapp/generatedloadingscreenwidget2/GeneratedLoadingscreenWidget2.dart';
+import 'package:flutterapp/salvadores_verdesapp/generatedloginorregisterwidget/GeneratedLoginorregisterWidget.dart';
+import 'package:flutterapp/salvadores_verdesapp/generatedtextwidget/GeneratedTextWidget.dart';
+import 'package:flutterapp/salvadores_verdesapp/generatedregistration1widget/GeneratedRegistration1Widget.dart';
+import 'package:flutterapp/salvadores_verdesapp/generatedregistration3widget/GeneratedRegistration3Widget.dart';
+import 'package:flutterapp/salvadores_verdesapp/generatedregistration2widget/GeneratedRegistration2Widget.dart';
+import 'package:flutterapp/salvadores_verdesapp/generatedcheckcirclewidget/GeneratedCheckcircleWidget.dart';
+import 'package:flutterapp/salvadores_verdesapp/generatediphone11promax3widget/GeneratedIPhone11ProMax3Widget.dart';
+import 'package:flutterapp/salvadores_verdesapp/generatediphone11promax8widget/GeneratedIPhone11ProMax8Widget.dart';
+import 'package:flutterapp/salvadores_verdesapp/generatediphone11promax12widget/GeneratedIPhone11ProMax12Widget.dart';
+import 'package:flutterapp/salvadores_verdesapp/generatediphone11promax4widget/GeneratedIPhone11ProMax4Widget.dart';
+import 'package:flutterapp/salvadores_verdesapp/generatediphone11promax5widget/GeneratedIPhone11ProMax5Widget.dart';
+import 'package:flutterapp/salvadores_verdesapp/generatediphone11promax11widget/GeneratedIPhone11ProMax11Widget.dart';
+import 'package:flutterapp/salvadores_verdesapp/generatediphone11promax13widget/GeneratedIPhone11ProMax13Widget.dart';
+import 'package:flutterapp/salvadores_verdesapp/generatediphone11promax9widget/GeneratedIPhone11ProMax9Widget.dart';
+import 'package:flutterapp/salvadores_verdesapp/generatediphone11promax10widget/GeneratedIPhone11ProMax10Widget.dart';
+import 'package:flutterapp/salvadores_verdesapp/generatediphone11promax7widget/GeneratedIPhone11ProMax7Widget.dart';
+import 'package:flutterapp/salvadores_verdesapp/generatediphone11promax6widget/GeneratedIPhone11ProMax6Widget.dart';
+import 'package:flutterapp/salvadores_verdesapp/generatediphone11promax14widget/GeneratedIPhone11ProMax14Widget.dart';
+import 'package:flutterapp/salvadores_verdesapp/generatedtruckwidget/GeneratedTruckWidget.dart';
+import 'package:flutterapp/salvadores_verdesapp/generatedshoppingbasketwidget/GeneratedShoppingbasketWidget.dart';
+import 'package:flutterapp/salvadores_verdesapp/generatedpaperclipwidget/GeneratedPaperclipWidget.dart';
+import 'package:flutterapp/salvadores_verdesapp/generatedpaperplanewidget/GeneratedPaperplaneWidget.dart';
+import 'package:flutterapp/salvadores_verdesapp/generatedarrowleftwidget/GeneratedArrowleftWidget.dart';
+
+void main() {
+  runApp(Salvadores_verdesApp());
+}
+
+class Salvadores_verdesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedLoadingscreenWidget',
+      routes: {
+        '/GeneratedLoadingscreenWidget': (context) =>
+            GeneratedLoadingscreenWidget(),
+        '/GeneratedLoadingscreenWidget1': (context) =>
+            GeneratedLoadingscreenWidget1(),
+        '/GeneratedFrame4Widget': (context) => GeneratedFrame4Widget(),
+        '/GeneratedLoadingscreenWidget2': (context) =>
+            GeneratedLoadingscreenWidget2(),
+        '/GeneratedLoginorregisterWidget': (context) =>
+            GeneratedLoginorregisterWidget(),
+        '/GeneratedTextWidget': (context) => GeneratedTextWidget(),
+        '/GeneratedRegistration1Widget': (context) =>
+            GeneratedRegistration1Widget(),
+        '/GeneratedRegistration3Widget': (context) =>
+            GeneratedRegistration3Widget(),
+        '/GeneratedRegistration2Widget': (context) =>
+            GeneratedRegistration2Widget(),
+        '/GeneratedCheckcircleWidget': (context) =>
+            GeneratedCheckcircleWidget(),
+        '/GeneratedIPhone11ProMax3Widget': (context) =>
+            GeneratedIPhone11ProMax3Widget(),
+        '/GeneratedIPhone11ProMax8Widget': (context) =>
+            GeneratedIPhone11ProMax8Widget(),
+        '/GeneratedIPhone11ProMax12Widget': (context) =>
+            GeneratedIPhone11ProMax12Widget(),
+        '/GeneratedIPhone11ProMax4Widget': (context) =>
+            GeneratedIPhone11ProMax4Widget(),
+        '/GeneratedIPhone11ProMax5Widget': (context) =>
+            GeneratedIPhone11ProMax5Widget(),
+        '/GeneratedIPhone11ProMax11Widget': (context) =>
+            GeneratedIPhone11ProMax11Widget(),
+        '/GeneratedIPhone11ProMax13Widget': (context) =>
+            GeneratedIPhone11ProMax13Widget(),
+        '/GeneratedIPhone11ProMax9Widget': (context) =>
+            GeneratedIPhone11ProMax9Widget(),
+        '/GeneratedIPhone11ProMax10Widget': (context) =>
+            GeneratedIPhone11ProMax10Widget(),
+        '/GeneratedIPhone11ProMax7Widget': (context) =>
+            GeneratedIPhone11ProMax7Widget(),
+        '/GeneratedIPhone11ProMax6Widget': (context) =>
+            GeneratedIPhone11ProMax6Widget(),
+        '/GeneratedIPhone11ProMax14Widget': (context) =>
+            GeneratedIPhone11ProMax14Widget(),
+        '/GeneratedTruckWidget': (context) => GeneratedTruckWidget(),
+        '/GeneratedShoppingbasketWidget': (context) =>
+            GeneratedShoppingbasketWidget(),
+        '/GeneratedPaperclipWidget': (context) => GeneratedPaperclipWidget(),
+        '/GeneratedPaperplaneWidget': (context) => GeneratedPaperplaneWidget(),
+        '/GeneratedArrowleftWidget': (context) => GeneratedArrowleftWidget(),
+      },
+    );
+  }
+}
